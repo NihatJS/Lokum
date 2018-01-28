0b49686f76e710a315676d966645e71a5293a958
NihatJS <34793252+NihatJS@users.noreply.github.com>
2018-01-28 13:20:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,15 @@ client.on('message', msg => {
             		.setColor(`#${color}`);
      	msg.channel.send({embed: embed});
 	}
-  
+    if (message.content == "^clear") {
+        if (message.member.hasPermission("MANAGE_MESSAGES")) {
+            message.channel.fetchMessages()
+               .then(function(list){
+                    message.channel.bulkDelete(list);
+                }, function(err){message.channel.send("ERROR: ERROR CLEARING CHANNEL.")})
+    }
+
+});
   
 });
 
