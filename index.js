a1ace38491fde803a5299afef6ac572ccc759d80
NihatJS <34793252+NihatJS@users.noreply.github.com>
2018-01-29 14:19:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,55 +1,15 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
+const http = require('http');
 
-client.on('ready', () => {
-  client.user.setGame("l!yardım | Yumuşacığım :)");
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+	res.end('Hello World\n');
+  res.end('Hello World\n');
 });
 
-client.on('message', msg => {
-	
-
-
-  
-	// Yardım
-	if (msg.content === 'l!yardım') {
-		const embed = new Discord.RichEmbed()
-		.setColor(0x22e172)
-		.setTitle("Lokum Komut listesi:")
-		.addField("l!yardım", "Komut listesini gösterir")
-		.addField("l!hakkında", "Botun bilgilerini gösterir")
-		.addField("l!rastgelehex", "Rastgele hex kodu üretir ve size gösterir")
-		msg.channel.send({embed})
-	   console.log(msg.channel.name + " adlı kanalda Komut kullanıldı: l!yardım")
-	}
-	// Yardım
-  
-  
-  
-  	if (msg.content === 'l!hakkımda') {
-    		msg.channel.sendMessage("`Lokum Bot | V1.0 By NihatJS`");
-    		msg.channel.send("`"+Math.floor(client.ping)+"ms`")
- 	 }
-  
-  
-	if (msg.content === 'l!rastgelehex') {
-      		let color = ((1 << 24) * Math.random() | 0).toString(16); //Rastgele hex kodu üretir.
-      		let embed = new Discord.RichEmbed() //Embedi oluşturur.
-            		.setTitle(`#${color}`)
-            		.setColor(`#${color}`);
-     	msg.channel.send({embed: embed});
-	}
-    if (message.content == "^clear") {
-        if (message.member.hasPermission("MANAGE_MESSAGES")) {
-            message.channel.fetchMessages()
-               .then(function(list){
-                    message.channel.bulkDelete(list);
-                }, function(err){message.channel.send("ERROR: ERROR CLEARING CHANNEL.")})
-    }
-
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
-  
-});
-
-
-
-client.login(process.env.token);
